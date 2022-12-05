932c9862ce084c29f160df5ec7ac387960e2f0e3
Lianne <huttlianne@gmail.com>
2022-12-05 00:43:01 +0000

    Add project files.

---
+++ AdventOfCode2022/Program.cs
@@ -0,0 +1,13 @@
+﻿using System.Diagnostics.Metrics;
+using System.Reflection.PortableExecutable;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+         Day1.day1();// 206780
+        //Day2.Day2part2(); //12683
+        //Day3.day3Part2(); //7831  2683
+    }
+}
